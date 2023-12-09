31c3d4f8ce28d30b0ab08eae597afafbe796db3c
mmulcahy1 <meagan.mulcahy@uconn.edu>
2023-12-09 16:40:21 -0500

    added solutions for day 9

---
+++ 2023/solutions/day09part2.py
@@ -0,0 +1,29 @@
+import sys
+import re
+lines = [line.replace('\n','') for line in open(sys.argv[1],'r').readlines()]
+
+result = 0
+
+for line in lines:
+    sequences = []
+    currSeq = [int(i) for i in re.findall('-?\d+\.?\d*', line)]    #get initial sequence
+    sequences.append(currSeq)
+    while(not all(s==0 for s in currSeq)):  #check if current sequence is all zeroes
+        nextSeq = []
+        for i in range(0, len(currSeq)-1):              #get next sequence
+            nextSeq.append(currSeq[i+1] - currSeq[i])
+            
+        sequences.append(nextSeq)
+        currSeq = nextSeq
+        
+    prevVal = 0
+    #get the previous value for each sequence
+    for j in range(len(sequences) - 1, -1, -1):
+        prevVal = sequences[j][0] - prevVal
+
+    result += prevVal
+        
+    
+print(result)
+
+    
